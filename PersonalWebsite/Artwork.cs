fb73fce6e79592dd1b957aa7edc0b4900ef8f767
cainy-a <cainatkinson98@gmail.com>
2020-07-09 13:42:26 +0100

    Vector Gallery done!!

--- PersonalWebsite/Artwork.cs
+++ PersonalWebsite/Artwork.cs
@@ -4,15 +4,20 @@ namespace PersonalWebsite
 {
     class Artwork
     {
-        public Artwork(string name, string relativeSvgUrl, string relativePngUrl)
+        public Artwork(string name, string relativeSvgUrl, string relativePngUrl, int width, int height)
         {
             Name = name;
             RelativeSvgUrl = relativeSvgUrl;
             RelativePngUrl = relativePngUrl;
+            Width = width;
+            Height = height;
         }
 
         public string RelativeSvgUrl;
         public string RelativePngUrl;
         public string Name;
+        public int Width;
+        public int Height;
+        public string Size => $"{Width} x {Height}";
     }
 }
