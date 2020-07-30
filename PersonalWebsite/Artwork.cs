2458d5bd14afc40837caf107c733047851bf0dbf
cainy-a <cainatkinson98@gmail.com>
2020-07-30 20:57:47 +0100

    Port Full site over

---
+++ PersonalWebsite/Artwork.cs
@@ -0,0 +1,21 @@
+namespace PersonalWebsite
+{
+    public class Artwork
+    {
+        public Artwork(string name, string relativeSvgUrl, string relativePngUrl, int width, int height)
+        {
+            Name = name;
+            RelativeSvgUrl = relativeSvgUrl;
+            RelativePngUrl = relativePngUrl;
+            Width = width;
+            Height = height;
+        }
+
+        public string RelativeSvgUrl;
+        public string RelativePngUrl;
+        public string Name;
+        public int Width;
+        public int Height;
+        public string Size => $"{Width} x {Height}";
+    }
+}
