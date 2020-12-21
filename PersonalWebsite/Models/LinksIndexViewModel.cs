36d2e1cd9defce40f653db6c55e836f15c1a2ffd
cainy-a <cainatkinson98@gmail.com>
2020-12-21 13:00:22 +0000

    Link Hub

---
+++ PersonalWebsite/Models/LinksIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using PersonalWebsite.Classes;
+
+namespace PersonalWebsite.Models
+{
+	public class LinksIndexViewModel
+	{
+		public Dictionary<string, AccountLink> AccountLinks;
+	}
+}
