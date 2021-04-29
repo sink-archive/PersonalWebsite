1a6f0c07bbc9d6733021fc3ef6d4d773436d59e5
cainy-a <yellowsink@protonmail.com>
2021-04-29 08:13:31 +0100

    stuff

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<link href="../lib/css/styles.css" rel="stylesheet" />
+</svelte:head>
+
+<nav>
+	<a href=".">Home</a>
+	<a href="rhythm">Rhythm Games</a>
+	<a href="links">Links</a>
+</nav>
+
+<slot/>
