95890c172a2480d735b9108164cdca7653c65483
cainy-a <cainy-a@users.noreply.github.com>
2021-05-01 18:45:59 +0100

    discord modding

---
+++ src/routes/project/discord-modding.svelte
@@ -0,0 +1,47 @@
+<script>
+	import GithubIcon from '../../components/GithubIcon.svelte';
+</script>
+
+<h1>Discord Client Mods, Themes, and Plugins</h1>
+
+<p class="subtitle">All things client mods for Discord (that I've made).</p>
+
+<h3>Diskai <GithubIcon repo="Diskai" /></h3>
+<p>
+	Diskai is my main theme, and where my main development effort goes.
+	It is designed to look like the Monokai colour scheme,
+	but ended up very different to my inital expectation.
+</p>
+<p>
+	Diskai's big feature is CUSTOMISATION - <em>everything is customisable</em>.
+	Want to change your accent colour? Go for it. Change the background colours? Sure!
+	Wanna redefine the ENTIRE colour scheme? No problemo.
+</p>
+<p>
+	I planned to make <em>Diskai Schemes</em>
+	(a premade set of non-monokai inspired colour schemes for Diskai),
+	but as of writing this page (2021-02-27 18:55 GMT), I haven't actually started yet... 
+</p>
+<p>
+	<a href="//github.com/cainy-a/Diskai/#i-want-to-customise-it">Check out the full customisation guide.</a></p>
+<p>
+	It also has a sizable list of optional features, including
+	a better user card,
+	strikethrough muted channels,
+	and compact mode provided by SmolCord (with some extra integration with Diskai's features)
+</p>
+
+<h3>SmolCord <GithubIcon repo="SmolCord" /></h3>
+<p>Ever feel like Discord wastes a lot of space? Yeah, me too.</p>
+<p>
+	SmolCord is a theme designed specifically to make Discord waste as little space as possible.
+	It does this by removing unneeded space around certain things like
+	context menus,
+	the user and channel lists,
+	and the server & chat headers.
+</p>
+
+<h3>LunaCord <GithubIcon repo="LunaCord" /></h3>
+<p>
+	Lunacord is a faithful recreation of the Windows XP Luna Blue theme, based on XP.css.
+</p>
