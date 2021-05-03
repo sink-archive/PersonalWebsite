dd43bf917487964a138c405fada25bd16f92fd17
cainy-a <yellowsink@protonmail.com>
2021-05-03 22:21:13 +0100

    Start of gallery

---
+++ src/components/VectorCard.svelte
@@ -0,0 +1,59 @@
+<script>
+ export let path;
+ export let name;
+</script>
+
+<div class="card">
+    <img src="//yellowsink.herokuapp.com/abstract_art/bitmap/{path}.png"
+         class="image"
+         alt="Preview of Artwork: '{name}'">
+    <p class="name">{name}</p>
+    <div class="buttons">
+        <a href="/abstract_art/vector/{path}.svg"
+           target="_blank"
+           download
+           class="button">SVG</a>
+        <a href="/abstract_art/bitmap/{path}.png"
+           target="_blank"
+           download
+           class="button">PNG</a>
+    </div>
+</div>
+
+<style>
+ .card {
+     width: 18rem;
+     margin: .5rem;
+     border: 1px solid lightslategray;
+     border-radius: .25rem;
+     padding: .5rem;
+ }
+
+ .image {
+     object-fit: contain;
+     height: 10rem !important;
+     display: block;
+     margin-left: auto;
+     margin-right: auto;
+ }
+
+ .buttons {
+     display: flex;
+ }
+
+ .button {
+     width: 100%;
+     background: 0;
+     border: 1px darkcyan solid;
+     border-radius: .25rem;
+     margin: .25rem;
+     text-align: center;
+     color: darkcyan;
+ }
+
+ .name {
+     font-weight: lighter;
+     font-size: 1.5em;
+     margin: .5em 0;
+ }
+</style>
---
+++ src/routes/gallery.svelte
@@ -0,0 +1,20 @@
+<script>
+    import VectorCard from '../components/VectorCard.svelte';
+</script>
+
+<h1>Some vector images ig</h1>
+<img src="/abstract_art/bitmap/avatar/avatar_classic.jpeg"
+     style="width:7rem;height:7rem;margin-left:1rem;"
+     alt="My old Profile Picture"
+     align="right">
+
+<p class="subtitle">
+    Inspired by this image, which was my profile pic from 2014(ish)
+    to the beginning of July, 2020. It was the basis for my new set
+    of profile pictures, which are currently in use.
+    Check the homepage to see my current Gravatar.:
+</p>
+
+<div style="display: flex; flex-flow: row wrap; justify-content: space-evenly">
+    <VectorCard path="avatar/avatar" name="Main avatar" />
+</div>
