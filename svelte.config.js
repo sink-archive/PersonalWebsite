2b831348dc2c525676f30092bdf1d332be245ce6
cainy-a <yellowsink@protonmail.com>
2021-05-04 19:58:40 +0100

    vercel stuff idk

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-const preprocess = require('svelte-preprocess');
+import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+    adapter: vercel()
 	}
 };
+
+export default config;
